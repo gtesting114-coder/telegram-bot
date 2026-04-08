5db67e434b8ca16343d558afe575981fbe9a6e1f
gtesting114-coder <gtesting114@gmail.com>
2026-04-08 20:00:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,39 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
 import os
+import yt_dlp
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
 TOKEN = os.getenv("8210783325:AAFoWXeLK_CLhhWM7nSOdLfjEGuYQd4rvlE")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello bro 😎 Bot is live!")
+    await update.message.reply_text("Send me the video link 😎")
+
+async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = update.message.text
+
+    await update.message.reply_text("Downloading... ⏳")
+
+    ydl_opts = {
+        'outtmpl': 'video.%(ext)s',
+        'format': 'mp4'
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+
+        for file in os.listdir():
+            if file.endswith(".mp4"):
+                with open(file, 'rb') as f:
+                    await update.message.reply_video(f)
+                os.remove(file)
+
+    except Exception as e:
+        await update.message.reply_text("you didn't follow lgd.efx")
 
 app = ApplicationBuilder().token(TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT, download))
 
 app.run_polling()
