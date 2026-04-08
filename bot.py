329f4fb7c5cd04dc0b23b0a04bc0fa613d4233b4
gtesting114-coder <gtesting114@gmail.com>
2026-04-08 19:24:04 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,12 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8210783325:AAFoWXeLK_CLhhWM7nSOdLfjEGuYQd4rvlE"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello bro 😎 Bot is live!")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
